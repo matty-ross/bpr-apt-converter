0abd4bf9243859c2038fc34a0573f52e752976c5
Matty <54020396+matty-ross@users.noreply.github.com>
2022-10-20 20:23:01 +0200

    Add files via upload

--- converter.py
+++ converter.py
@@ -12,6 +12,36 @@ def swap(fp: BinaryIO, fmt: str) -> int:
     return struct.unpack(fmt, buff)[0]
 
 
+def convert_frame(fp: BinaryIO, apt_data_offset: int, frame_offset: int) -> None:
+    fp.seek(apt_data_offset + frame_offset, os.SEEK_SET)
+    item_count = swap(fp, "<L")
+    items_offset = swap(fp, "<L")
+    for i in range(item_count):
+        fp.seek(apt_data_offset + items_offset + i * 0x4, os.SEEK_SET)
+        frame_item_offset = swap(fp, "<L")
+        if frame_item_offset == 0:
+            continue
+        fp.seek(apt_data_offset + frame_item_offset)
+        frame_item_type = swap(fp, "<L")
+
+        if frame_item_type == 1: # action
+            pass
+        elif frame_item_type == 2: # frame label
+            pass
+        elif frame_item_type == 3: # place object
+            pass
+        elif frame_item_type == 4: # remove object
+            swap(fp, "<L")
+        elif frame_item_type == 5: # background color
+            swap(fp, "<L")
+        elif frame_item_type == 6: # start sound
+            assert False, "Should be unused"
+        elif frame_item_type == 7: # start sound stream
+            assert False, "Should be unused"
+        elif frame_item_type == 8: # init action
+            pass
+
+
 def convert_character(fp: BinaryIO, apt_data_offset: int, character_offset: int) -> None:
     fp.seek(apt_data_offset + character_offset, os.SEEK_SET)
     character_type = swap(fp, "<L")
@@ -20,30 +50,75 @@ def convert_character(fp: BinaryIO, apt_data_offset: int, character_offset: int)
     swap(fp, "<H")
     swap(fp, "<L")
 
-    if character_type == 1:
-        pass
-    elif character_type == 2:
-        pass
-    elif character_type == 3:
-        pass
-    elif character_type == 4:
-        pass
-    elif character_type == 5:
-        pass
-    elif character_type == 6:
-        pass
-    elif character_type == 7:
-        pass
-    elif character_type == 8:
-        pass
-    elif character_type == 9:
-        pass
-    elif character_type == 10:
-        pass
-    elif character_type == 11:
-        pass
-    elif character_type == 12:
-        pass
+    if character_type == 1: # shape
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+    elif character_type == 2: # text
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+    elif character_type == 3: # font
+        swap(fp, "<L")
+        swap(fp, "<L")
+        assert swap(fp, "<L") == 0, "Should be unused"
+    elif character_type == 4: # button
+        assert False, "Should be unused"
+    elif character_type == 5: # sprite
+        frame_count = swap(fp, "<L")
+        frames_offset = swap(fp, "<L")
+        assert swap(fp, "<L") == 0, "Should be unused"
+        for i in range(frame_count):
+            convert_frame(fp, apt_data_offset, frames_offset + i * 0x8)
+    elif character_type == 6: # sound
+        assert False, "Should be unused"
+    elif character_type == 7: # image
+        swap(fp, "<L")
+    elif character_type == 8: # morph
+        assert False, "Should be unused"
+    elif character_type == 9: # movie
+        frame_count = swap(fp, "<L")
+        frames_offset = swap(fp, "<L")
+        assert swap(fp, "<L") == 0, "Should be unused"
+        fp.seek(0x8, os.SEEK_CUR) # skip characters
+        swap(fp, "<L")
+        swap(fp, "<L")
+        swap(fp, "<L")
+        import_count = swap(fp, "<L")
+        imports_offset = swap(fp, "<L")
+        export_count = swap(fp, "<L")
+        exports_offset = swap(fp, "<L")
+        assert swap(fp, "<L") == 0, "Should be unused"
+        fp.seek(apt_data_offset + imports_offset, os.SEEK_SET)
+        for _ in range(import_count):
+            swap(fp, "<L")
+            swap(fp, "<L")
+            swap(fp, "<L")
+            assert swap(fp, "<L") == 0, "Should be unused"
+        fp.seek(apt_data_offset + exports_offset, os.SEEK_SET)
+        for _ in range(export_count):
+            swap(fp, "<L")
+            swap(fp, "<L")
+        for i in range(frame_count):
+            convert_frame(fp, apt_data_offset, frames_offset + i * 0x8)
+    elif character_type == 10: # static text
+        assert False, "Should be unused"
+    elif character_type == 11: # none
+        assert False, "Should be unused"
+    elif character_type == 12: # video
+        assert False, "Should be unused"
 
 
 def convert(fp: BinaryIO) -> None:
